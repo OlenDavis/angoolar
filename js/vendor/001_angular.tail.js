dd1a53a4802df2c0882e3efca73558e1754afbc9
Olen Davis <spelledlikeitsounds@gmail.com>
2014-03-19 17:28:08 -0700

    chore(upgrade): Transcluded content of nested transcluded directives still requires notIsolated to function at all.

--- js/vendor/001_angular.tail.js
+++ js/vendor/001_angular.tail.js
@@ -6165,7 +6165,7 @@ function $CompileProvider($provide, $$sanitizeUriProvider) {
           var LOCAL_REGEXP = /^\s*([@=&])(\??)\s*(\w*)\s*$/;
           var $linkNode = jqLite(linkNode);
 
-          isolateScope = scope.$new(true);
+          isolateScope = scope.$new(!!!newIsolateScopeDirective.notIsolated);
 
           if (templateDirective && (templateDirective === newIsolateScopeDirective.$$originalDirective)) {
             $linkNode.data('$isolateScope', isolateScope) ;
